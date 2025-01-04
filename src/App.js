3f760e5fe326579b5d5920d9bfd9411516951754
Dedeepthika538 <dedeepthika.o@gmail.com>
2025-01-04 11:57:02 -0500

    Removed Unwanted Files

--- src/App.js
+++ src/App.js
@@ -1,48 +1,16 @@
 
 import './App.css';
-import { Header } from './components/Header';
 
-import { GlobalProvider ,GlobalContext} from './context/GlobalState';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import  Signup  from './components/Signup';
-import  Login  from './components/Login';
+import { GlobalProvider} from './context/GlobalState';
 import ExpenseTracker from './components/ExpenseTracker';
-import { useContext } from "react";
-
-const ProtectedRoute = ({ children }) => {
-  const { user } = useContext(GlobalContext);
-  return user ? children : <Navigate to="/signup" />;
-};
 
 function App() {
   return (
-    // <GlobalProvider>
-    //   <Header />
-    //   <div className="container">
-    //     <Signup />
-    //     <Login />
-    //     <ExpenseTracker/>
-    //   </div>
-    // </GlobalProvider>
-
     <GlobalProvider>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/tracker"
-          element={
-            <ProtectedRoute>
-              <ExpenseTracker />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
-    </Router>
-  </GlobalProvider>
+      <div className="container">
+        <ExpenseTracker/>
+      </div>
+    </GlobalProvider>
   );
 }
 
--- src/components/ExpenseTracker.js
+++ src/components/ExpenseTracker.js
@@ -3,10 +3,12 @@ import { Balance } from './Balance';
 import { IncomeExpenses } from './IncomeExpenses';
 import { TransactionList } from './TransactionList';
 import { AddTransaction } from './AddTransaction';
+import { Header } from './Header';
 
 export default function ExpenseTracker() {
     return (
         <div className="expense-tracker">
+             <Header />
             <Balance />
             <IncomeExpenses />
             <TransactionList />
